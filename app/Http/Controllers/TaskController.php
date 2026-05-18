f8f03a1993f75560023edf3ecd681a20a85adad8
r.koga <r.koga@core-tech.jp>
2026-05-18 15:03:26 +0900

    削除機能の完成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -68,6 +68,7 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return redirect()->route('tasks.index');
     }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,6 +16,11 @@
     <p>ステータス：{{$task->status_label}}</p>
     <p>期限：{{$task->due_date}}</p>
     <a href="{{route('tasks.show',$task)}}">詳細画面</a>
+    <form action="{{route('tasks.destroy',$task)}}" method="post">
+        @csrf
+        @method('DELETE')
+        <button type="submit" onclick="return confirm('本当に削除しますか')">削除</button>
+    </form>
     @endforeach
 
 </body>
